79e9978e520f17a50d8e6a8d0025786174081af2
Nathan Do <5110105+nathando91@users.noreply.github.com>
2024-02-29 12:28:10 +0700

    Update install-script.sh

--- install-script.sh
+++ install-script.sh
@@ -1,16 +1,41 @@
 #!/bin/sh
 
-# Định nghĩa URL của script hook
-HOOK_SCRIPT_URL="https://github.com/nathando91/install-git-hook/blob/main/hook.sh"
+# Define the path to the global git hooks directory
+GIT_HOOKS_DIR="$HOME/global_git_hooks"
 
-# Đường dẫn tới thư mục git hooks cục bộ
-# Thay đổi "your_repo" bằng đường dẫn tới repo git cục bộ của bạn
-GIT_HOOKS_PATH="~/global_git_hooks/pre-commit"
+# Ensure the global git hooks directory exists
+mkdir -p "$GIT_HOOKS_DIR"
 
-# Tải script hook và lưu vào thư mục git hooks cục bộ
-curl -Ls $HOOK_SCRIPT_URL -o $GIT_HOOKS_PATH
+# Define the path to the pre-commit hook within the global git hooks directory
+GIT_HOOKS_PATH="$GIT_HOOKS_DIR/pre-commit"
 
-# Đặt quyền thực thi cho script
-chmod +x $GIT_HOOKS_PATH
+# Provide the content of the pre-commit hook script
+HOOK_SCRIPT_CONTENT='#!/bin/sh
 
-echo "Git hook script has been installed."
+# Your pre-commit hook script content here
+# For example, a simple user check:
+local_user=$(git config --local user.name)
+local_email=$(git config --local user.email)
+
+if [ -z "$local_user" ] || [ -z "$local_email" ]; then
+    echo "Warning: Git local user configuration is not set."
+    echo "Please configure your Git local user name and email for this project."
+    echo "Use the following commands:"
+    echo "git config --local user.name \"Your Name\""
+    echo "git config --local user.email \"your_email@example.com\""
+    exit 1
+fi
+'
+
+# Create or overwrite the pre-commit hook with the given content
+echo "$HOOK_SCRIPT_CONTENT" > "$GIT_HOOKS_PATH"
+
+# Set execute permission for the pre-commit hook
+chmod +x "$GIT_HOOKS_PATH"
+
+echo "Git hook script has been installed at $GIT_HOOKS_PATH"
+
+# Set the global Git configuration to use the global git hooks directory
+git config --global core.hooksPath "$HOME/global_git_hooks"
+
+echo "Global Git hooks path has been set to $HOME/global_git_hooks"
